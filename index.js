5f2e96cad9fc93798a9ac00e42873786268ae2fc
Jacques Kang <jkang.perso@gmail.com>
2019-08-21 09:51:26 +0200

    initial implementation

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+    console.log('Launching browser...');
+    const browser = await puppeteer.launch();
 
-  await browser.close();
+    console.log('Showing booking page...');
+    const page = await browser.newPage();
+    const response1 = await page.goto('http://www.hauts-de-seine.gouv.fr/booking/create/11776/0');
+    if (!response1.ok()) {
+        console.log('Page showing error (see screenshot). Exiting');
+        await page.screenshot({ path: 'screenshot.png' });
+        return;
+    }
+
+    console.log('Clicking accept condition checkbox...');
+    await page.click('input#condition');
+
+    console.log('Clicking next button and waiting for navigation...');
+    const [response2, _] = await Promise.all([
+        page.waitForNavigation(),
+        page.click('input[name=nextButton]'),
+    ]);
+
+    if (!response2.ok()) {
+        console.log('Page showing error (see screenshot). Exiting');
+        await page.screenshot({ path: 'screenshot.png' });
+        return;
+    }
+
+    var formText = await page.$eval('form#FormBookingCreate', el => el.innerText);
+    console.log('Showing: ' + formText);
+    if (formText.search('n\'existe plus de plage') >= 0)
+    {
+        console.log('No place available. exiting...');
+        return;
+    }
+
+    console.log('AHHHHHHH');
+
+    await page.screenshot({ path: 'screenshot.png' });
+    await browser.close();
 })();
