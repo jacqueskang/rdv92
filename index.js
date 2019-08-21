8e3844c3410d4d7fc93eda42a2e11b7799df9857
Jacques Kang <jkang.perso@gmail.com>
2019-08-21 10:31:24 +0200

    send email when place found

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const puppeteer = require('puppeteer');
+const nodemailer = require('nodemailer');
+
+var args = process.argv.slice(2);
 
 (async () => {
     console.log('Launching browser...');
@@ -8,9 +11,10 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     const response1 = await page.goto('http://www.hauts-de-seine.gouv.fr/booking/create/11776/0');
     if (!response1.ok()) {
-        console.log('Page showing error (see screenshot). Exiting');
+        console.log('Page showing error (see screenshot). Exiting...');
         await page.screenshot({ path: 'screenshot.png' });
-        return;
+        browser.close();
+        process.exit(-1);
     }
 
     console.log('Clicking accept condition checkbox...');
@@ -23,21 +27,47 @@ const puppeteer = require('puppeteer');
     ]);
 
     if (!response2.ok()) {
-        console.log('Page showing error (see screenshot). Exiting');
+        console.log('Page showing error (see screenshot). Exiting...');
         await page.screenshot({ path: 'screenshot.png' });
-        return;
+        browser.close();
+        process.exit(-1);
     }
-
-    var formText = await page.$eval('form#FormBookingCreate', el => el.innerText);
-    console.log('Showing: ' + formText);
-    if (formText.search('n\'existe plus de plage') >= 0)
-    {
-        console.log('No place available. exiting...');
-        return;
-    }
-
-    console.log('AHHHHHHH');
 
     await page.screenshot({ path: 'screenshot.png' });
-    await browser.close();
+    var formText = await page.$eval('form#FormBookingCreate', el => el.innerText);
+    console.log('"' + formText + '"');
+    if (formText.search('n\'existe plus de plage') >= 0) {
+        console.log('No place available. exiting...');
+        browser.close();
+        process.exit(-1);
+    }
+
+    console.log('PLACE DISPONIBLE!!! Sending email...');
+    var transporter = nodemailer.createTransport({
+        host: 'email-smtp.eu-west-1.amazonaws.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'AKIA5PXSB3J4ZUBJNISC',
+            pass: args[0]
+        }
+    });
+
+    transporter.sendMail({
+        from: 'jacques.kang@sweeyoo.com',
+        to: 'jacques.kang@sweeyoo.com',
+        subject: 'PLACE DISPONIBLE!!!',
+        text: 'http://www.hauts-de-seine.gouv.fr/booking/create/11776/0'
+    }, function (error, info) {
+        if (error) {
+            console.log('Error while sending email: ' + error);
+            browser.close();
+            process.exit(-1);
+        } else {
+            console.log('Email sent: ' + info.response);
+            console.log('Closing browser and exiting...');
+            browser.close();
+            process.exit(0);
+        }
+    });
 })();
