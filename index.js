dd25b03917c76036727757931d68a6854bd415db
Jacques Kang <jacques.kang@voluntis.com>
2020-06-22 20:23:10 +0200

    update

--- index.js
+++ index.js
@@ -11,74 +11,101 @@ logger.level = 'debug';
 
 var args = process.argv.slice(2);
 
+async function checkResponse(page, browser, response) {
+    if (!response.ok()) {
+        logger.error('Page showing error (see last-error.png). Exiting...');
+        await page.screenshot({ path: 'last-error.png' });
+        browser.close();
+        process.exit(-1);
+    }
+}
+
+async function clickButtonAndWaitForNavigation(page, browser, selector) {
+    const [response, _] = await Promise.all([
+        page.waitForNavigation(),
+        page.click(selector),
+    ]);
+    await checkResponse(page, browser, response);
+}
+
+async function sendEmailAndQuit(page, browser, guichet, address) {
+    logger.info(`PLACE DISPONIBLE!!! Sending email to '${address}'...`);
+    var transporter = nodemailer.createTransport({
+        host: 'email-smtp.eu-west-1.amazonaws.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'AKIA5PXSB3J4TYLESH7G',
+            pass: args[0]
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: 'jacques.kang@sweeyoo.com',
+        to: address,
+        subject: `PLACE DISPONIBLE : guichet ${guichet} !!!`,
+        text: 'http://www.hauts-de-seine.gouv.fr/booking/create/12083'
+    });
+
+    logger.info('Email sent. Taking screenshot (last-available.png) and exiting...');
+    await page.screenshot({ path: 'last-available.png' });
+
+    browser.close();
+    process.exit(0);
+}
+
 (async () => {
     logger.info('Launching browser...');
     const browser = await puppeteer.launch();
 
     logger.info('Showing booking page...');
     const page = await browser.newPage();
-    const response1 = await page.goto('http://www.hauts-de-seine.gouv.fr/booking/create/11776/0');
-    if (!response1.ok()) {
-        logger.error('Page showing error (see last-error.png). Exiting...');
-        await page.screenshot({ path: 'last-error.png' });
-        browser.close();
-        process.exit(-1);
-    }
+
+    var response = await page.goto('http://www.hauts-de-seine.gouv.fr/booking/create/12083');
+    await checkResponse(page, browser, response)
 
     logger.info('Clicking accept condition checkbox...');
     await page.click('input#condition');
 
-    logger.info('Clicking next button and waiting for navigation...');
-    const [response2, _] = await Promise.all([
-        page.waitForNavigation(),
-        page.click('input[name=nextButton]'),
-    ]);
+    logger.info('Clicking next button...');
+    await clickButtonAndWaitForNavigation(page, browser, 'input[name=nextButton]');
 
-    if (!response2.ok()) {
-        logger.error('Page showing error (see last-error.png). Exiting...');
-        await page.screenshot({ path: 'last-error.png' });
-        browser.close();
-        process.exit(-1);
+    var guichets = [{
+        name: '1B',
+        id: 'planning15255'
+    }, {
+        name: '4B',
+        id: 'planning14673'
+    }, {
+        name: '3B',
+        id: 'planning14806'
+    }, {
+        name: '2B',
+        id: 'planning14932'
+    }];
+
+    for (const guichet of guichets) {
+        logger.info(`Clicking guichet '${guichet.name}'...`);
+        await page.click(`input#${guichet.id}`);
+
+        logger.info('Clicking next button...');
+        await clickButtonAndWaitForNavigation(page, browser, 'input[name=nextButton]');
+
+        var formText = await page.$eval('form#FormBookingCreate', el => el.innerText);
+        logger.info(`Text read: '${formText}'`);
+        if (formText.search('n\'existe plus de plage!') < 0) {
+            await sendEmailAndQuit(page, browser, '1B', 'jkang.perso@gmail.com');
+        }
+
+        logger.info(`No place available for guichet '${guichet.name}'. Clicking on finish button...`);
+        await clickButtonAndWaitForNavigation(page, browser, 'input[name=finishButton]');
+
+        logger.info('Clicking accept condition checkbox...');
+        await page.click('input#condition');
+
+        logger.info('Clicking next button...');
+        await clickButtonAndWaitForNavigation(page, browser, 'input[name=nextButton]');
     }
 
-    var formText = await page.$eval('form#FormBookingCreate', el => el.innerText);
-    logger.info('"' + formText + '"');
-    if (formText.search('n\'existe plus de plage') >= 0) {
-        logger.info('No place available (see last-unavailable). exiting...');
-        await page.screenshot({ path: 'last-unavailable.png' });
-        browser.close();
-        process.exit(-1);
-    }
-
-    logger.info('PLACE DISPONIBLE!!! Sending email...');
-    var transporter = nodemailer.createTransport({
-        host: 'email-smtp.eu-west-1.amazonaws.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: 'AKIA5PXSB3J4ZUBJNISC',
-            pass: args[0]
-        }
-    });
-
-    transporter.sendMail({
-        from: 'jacques.kang@sweeyoo.com',
-        to: 'jacques.kang@sweeyoo.com',
-        subject: 'PLACE DISPONIBLE!!!',
-        text: 'http://www.hauts-de-seine.gouv.fr/booking/create/11776/0'
-    }, function (error, info) {
-        if (error) {
-            logger.error('Error while sending email: ' + error);
-            browser.close();
-            process.exit(-1);
-        } else {
-            logger.info('Email sent: ' + info.response);
-            logger.info('Closing browser and exiting...');
-            browser.close();
-            process.exit(0);
-        }
-    });
-
-    logger.info('Taking screenshot (last-available.png) and exiting...');
-    await page.screenshot({ path: 'last-available.png' });
+    logger.info(`No place available. Exiting...`);
 })();
